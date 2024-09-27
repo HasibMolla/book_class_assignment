85d48bf88e0ea9fa279d60faf454d8259bd9ff08
mdhasibmolla <mdhasibm700@gmail.com>
2024-09-27 14:14:29 +0600

    Book_cLass_assignment_Hasib

--- Book.dart
+++ Book.dart
@@ -2,21 +2,17 @@ class Book {
   String title;
   String author;
   int publicationYear;
-  int pagesRead;  // Non-nullable, needs to be initialized
-
+  int pagesRead;
   static int totalBooks = 0;
 
-  // Constructor with an initializer list to initialize pagesRead
   Book(this.title, this.author, this.publicationYear) : pagesRead = 0 {
-    totalBooks++;   // Increment totalBooks when a new book is created
+    totalBooks++;
   }
 
-  // Method to simulate reading
   void read(int pages) {
     pagesRead += pages;
   }
 
-  // Getter methods
   int getPagesRead() {
     return pagesRead;
   }
@@ -32,25 +28,20 @@ class Book {
   int getPublicationYear() {
     return publicationYear;
   }
-
-  // Calculate the age of the book
   int getBookAge() {
     return DateTime.now().year - publicationYear;
   }
 }
 
 void main() {
-  // Creating three Book objects
   Book book1 = Book("To Kill a Mockingbird", "Harper Lee", 1960);
   Book book2 = Book("The Great Gatsby", "F. Scott Fitzgerald", 1925);
   Book book3 = Book("1984", "George Orwell", 1949);
 
-  // Simulating reading pages
   book1.read(100);
   book2.read(50);
   book3.read(200);
 
-  // Printing details of each book
   print("Book 1:");
   print("Title: ${book1.getTitle()}");
   print("Author: ${book1.getAuthor()}");
@@ -75,6 +66,5 @@ void main() {
   print("Book Age: ${book3.getBookAge()} years");
   print("");
 
-  // Printing the total number of books created
   print("Total Books: ${Book.totalBooks}");
 }
